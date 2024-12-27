afa613c1a33ec80725847a380b5eae4df5086848
Roger C. Pao <3468574+rcpao@users.noreply.github.com>
2024-12-27 01:36:49 -0800

    Supermicro X13SEM-F, ipmitool, smart@fw40-070x

--- rpi-loop.sh
+++ rpi-loop.sh
@@ -21,43 +21,102 @@ function wait_for_fping() {
 } # wait_for_fping
 
 
+function wait_for_scp() {
+  local SCP_SRC=$1 # scp source filespec
+  local SCP_DST=$2 # scp destination filespec
+  local SCP_RC=$3 # the return code to wait for
+  local RC=-1
+
+  w=0
+  scp ${SCP_SRC} ${SCP_DST} > /dev/null 2>&1
+  RC=$?
+  while [ ${RC} -ne ${SCP_RC} ]; do
+    w=$((w+1))
+    echo -e -n "\b\b\b\b$w "
+    sleep 1  # Adjust the sleep interval as needed
+
+    scp ${SCP_SRC} ${SCP_DST} > /dev/null 2>&1
+    RC=$?
+  done
+
+  echo "scp ${SCP_SRC} ${SCP_DST} RC=${RC}" 
+} # wait_for_scp
+
+
+: <<'EOF'
 SSH_USER=rcpao
 SSH_HOST=ub24d-1t-mgef 
-SSH_ARP_A=$(arp -a ub24d-1t-mgef) # ub24d-1t-mgef.rs.paonet.org (172.18.128.200) at <incomplete> on eth0
-SSH_MAC=$(echo "$SSH_ARP_A" | cut -d ' ' -f 4) # <incomplete>
+# SSH_ARP_A=$(arp -a ub24d-1t-mgef) # ub24d-1t-mgef.rs.paonet.org (172.18.128.200) at <incomplete> on eth0
+# SSH_MAC="40:8d:5c:5e:40:36" # $(echo "$SSH_ARP_A" | cut -d ' ' -f 4) # <incomplete>
+  # wakeonlan does not turn on Gigabyte GA-Z97X-Gaming_7 
+EOF
+# : <<'EOF'
+SSH_USER=smart
+SSH_HOST=fw40-070x 
+# EOF
 
-NUM_LOOP=100
+: <<'EOF'
+BMC_IP="172.18.128.103" # 7C:C2:55:E6:F6:FC # Supermicro X14SBM-TF, CXL 2.0
+BMC_USER="ADMIN"
+BMC_PW="TGRMCKRDBV"
+EOF
+# : <<'EOF'
+BMC_IP="172.18.128.55" # 3c:ec:ef:d9:75:38 # Supermicro X13SEM-F
+BMC_USER="ADMIN"
+BMC_PW="ITUDHIWUYO"
+# EOF
+
+NUM_LOOP=2
+
+sudo apt -y install fping wakeonlan ipmitool
 
 for ((i=0; i < ${NUM_LOOP}; i++)); do
   echo "Iteration: $i of ${NUM_LOOP}"
   date
   echo "Power ON"
-  python rpi/ctrlOn.py
+  if [ ! -z "$BMC_IP" ]; then
+    ipmitool -H ${BMC_IP} -U ${BMC_USER} -P ${BMC_PW} power on
+  elif [ ! -z "$SSH_MAC" ]; then
+    # wakeonlan ${SSH_MAC} # 40:8d:5c:5e:40:36 # must have transmitted an Ethernet packet to populate the ARP table
+    wakeonlan ${SSH_MAC} # 40:8d:5c:5e:40:36
+  else
+    python rpi/ctrlOn.py # IoT Relay
+  fi
 
   #sleep 210 # wait for ${SSH_HOST} to start listening
-  # wakeonlan ${SSH_MAC} # 40:8d:5c:5e:40:36 # must have transmitted an Ethernet packet to populate the ARP table
-  wakeonlan 40:8d:5c:5e:40:36
+
   wait_for_fping ${SSH_HOST} 0
   echo "Host ${SSH_HOST} replied to fping" 
   # ${SSH_HOST} should now be listening 
+
+  wait_for_scp test1.sh ${SSH_USER}@${SSH_HOST}:bin/ 0
+  RC=$?
+  [ ${RC} -ne 0 ] && echo "error: scp test1.sh ${SSH_USER}@${SSH_HOST}:bin/ returned ${RC}" && exit ${RC}
 
   if [[ $i -eq 1 ]]; then
     time ssh ${SSH_USER}@${SSH_HOST} rm interval.txt log.txt
   fi
 
-  time scp test1.sh ${SSH_USER}@${SSH_HOST}:bin/
-  time ssh ${SSH_USER}@${SSH_HOST} bin/test1.sh
+  ssh ${SSH_USER}@${SSH_HOST} bin/test1.sh
+
   #sleep 60 # test1.sh 'shutdown -h now' should be off before power_off is run
   wait_for_fping ${SSH_HOST} 1
   echo "Host ${SSH_HOST} did not reply to fping" 
 
   set -x
+  sleep 20 # test1 should be off for a minimum of this many seconds to allow all hw devices to bleed power off
   echo "Power OFF"
-  sleep 10 # test1 should be off for a minimum of this many seconds to allow all hw devices to bleed power off
-  python rpi/ctrlOff.py
-  sleep 10 # test1 should be off for a minimum of this many seconds to allow all hw devices to bleed power off
-  python rpi/ctrlOff.py
-  sleep 10 # test1 should be off for a minimum of this many seconds to allow all hw devices to bleed power off
+  if [ ! -z "$BMC_IP" ]; then
+    ipmitool -H ${BMC_IP} -U ${BMC_USER} -P ${BMC_PW} power off
+  elif [ ! -z "$SSH_MAC" ]; then
+    # wakeonlan ${SSH_MAC} # 40:8d:5c:5e:40:36 # must have transmitted an Ethernet packet to populate the ARP table
+    #wakeonlan ${SSH_MAC} # 40:8d:5c:5e:40:36
+    echo "error: use 'ipmitool power off' or 'IoT Relay'"
+    exit 1
+  else
+    python rpi/ctrlOff.py # IoT Relay
+  fi
+  sleep 10 # test1 should be off for a minimum of this many seconds to allow all hardware to drain power off
   set +x
 done
 
--- test1.sh
+++ test1.sh
@@ -9,9 +9,18 @@ expr `cat $NUMFILE 2>/dev/null` + 1 >$NUMFILE
 ITERATION=$(cat $NUMFILE)
 echo "Iteration: $ITERATION" | tee -a $LOG
 
-echo "test 1" | tee -a $LOG
-echo "test 2" | tee -a $LOG
-echo "test 3" | tee -a $LOG
+echo "lsmem" | tee -a $LOG
+lsmem >> $LOG
+RC=$?
+[ ${RC} -ne 0 ] && echo "error: 'lsmem' returned ${RC}" && exit ${RC}
+
+echo "numactl -H" | tee -a $LOG
+numactl -H >> $LOG
+[ ${RC} -ne 0 ] && echo "error: 'numactl -H' returned ${RC}" && exit ${RC}
+
+# echo "test 1" | tee -a $LOG
+# echo "test 2" | tee -a $LOG
+# echo "test 3" | tee -a $LOG
 
 echo "shutdown -h now" | tee -a $LOG
 date | tee -a $LOG
