f642099c85105d15f62c7866b64589a337a39124
Roger C. Pao <roger.pao@smartm.com>
2024-12-30 21:47:05 -0800

    test1.sh: lsmem, numactl -H

--- test1.sh
+++ test1.sh
@@ -9,18 +9,53 @@ expr `cat $NUMFILE 2>/dev/null` + 1 >$NUMFILE
 ITERATION=$(cat $NUMFILE)
 echo "Iteration: $ITERATION" | tee -a $LOG
 
-echo "lsmem" | tee -a $LOG
-lsmem >> $LOG
+
+date | tee -a $LOG
+echo "lsmem" # | tee -a $LOG
+LLOG=lsmem.txt
+lsmem > $LLOG
 RC=$?
 [ ${RC} -ne 0 ] && echo "error: 'lsmem' returned ${RC}" && exit ${RC}
+cat $LLOG
+cat $LLOG >> $LOG
 
-echo "numactl -H" | tee -a $LOG
-numactl -H >> $LOG
+date | tee -a $LOG
+echo "numactl -H" # | tee -a $LOG
+LLOG=numactl_-H.txt
+numactl -H > $LLOG
+RC=$?
 [ ${RC} -ne 0 ] && echo "error: 'numactl -H' returned ${RC}" && exit ${RC}
+cat $LLOG
+cat $LLOG >> $LOG
+grep "node 1" $LLOG >> $LOG
+RC=$?
+[ ${RC} -ne 0 ] && echo "error: \'grep \"node 1\" $LLOG\' returned ${RC}" && exit ${RC}
+
+: <<'EOF'
+MLC=~/Downloads/mlc_v3.10/Linux/mlc
+date | tee -a $LOG
+echo "numactl --membind=0 $MLC"
+LLOG=numactl_--membind=0_mlc.txt
+numactl --membind=0 $MLC > $LLOG
+RC=$?
+[ ${RC} -ne 0 ] && echo "error: 'numactl -H' returned ${RC}" && exit ${RC}
+cat $LLOG
+cat $LLOG >> $LOG
+
+date | tee -a $LOG
+echo "numactl --membind=1 $MLC"
+numactl --membind=1 $MLC > $LLOG
+RC=$?
+[ ${RC} -ne 0 ] && echo "error: 'numactl -H' returned ${RC}" && exit ${RC}
+LLOG=numactl_--membind=1_mlc.txt
+cat $LLOG
+cat $LLOG >> $LOG
+EOF
 
 # echo "test 1" | tee -a $LOG
 # echo "test 2" | tee -a $LOG
 # echo "test 3" | tee -a $LOG
+
 
 echo "shutdown -h now" | tee -a $LOG
 date | tee -a $LOG
